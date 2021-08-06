b597b9832e5fbb23961b6d260dbf804b9e9dbe2c
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-08-06 09:35:27 +0200

    [UP] about us refactor

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -1,22 +1,22 @@
 import {
-    IonContent,
-    IonPage,
-    IonItem,
-    IonImg,
-    IonIcon,
-    IonLabel,
-    IonButton,
-    IonFooter,
-    IonToolbar,
-    IonCardContent,
-    IonCard,
-  } from '@ionic/react'
-  import React from 'react'
- 
-  import abb from '../../assets/img/abb.jpeg'
-  import smartphone from '../../assets/img/smartphone.png'
-  import { logoInstagram,logoFacebook,earthOutline,logoLinkedin,logoYoutube,
-    } from 'ionicons/icons'
+  IonContent,
+  IonPage,
+  IonItem,
+  IonImg,
+  IonIcon,
+  IonLabel,
+  IonButton,
+  IonFooter,
+  IonToolbar,
+  IonCardContent,
+  IonCard,
+} from '@ionic/react'
+import React from 'react'
+
+import abb from '../../assets/img/abb.jpeg'
+import smartphone from '../../assets/img/smartphone.png'
+import { logoInstagram,logoFacebook,earthOutline,logoLinkedin,logoYoutube,
+  } from 'ionicons/icons'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
@@ -38,99 +38,99 @@ import '@ionic/react/css/display.css'
 
 
 
-  const AboutUs = (props) => {
-    return (
+const AboutUs = (props) => {
+  return (
+    
+    <IonPage>
       
-      <IonPage>
-        
-          <IonToolbar>
-        <IonImg src={abb}
-            style={{maxWidth: "400px" , margin: "auto"}}
-            />
+        <IonToolbar>
+      <IonImg src={abb}
+          style={{maxWidth: "400px" , margin: "auto"}}
+          />
 
-            <IonImg src={smartphone}
-            style={{maxWidth: "150px" , margin: "auto"}}
-            />
-            </IonToolbar>
-       <IonToolbar>
-         
-       </IonToolbar>
+          <IonImg src={smartphone}
+          style={{maxWidth: "150px" , margin: "auto"}}
+          />
+          </IonToolbar>
+     <IonToolbar>
        
-        <IonContent >
-      
-            <IonToolbar  color="success">
-            
-              <div  style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-                }}>
-        <IonButton href="https://www.facebook.com/37100Lab.ComuneVerona" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
-              <IonIcon size="large" icon={ logoFacebook} />
-              <IonLabel ></IonLabel>
-            </IonButton>
-            </div>
-
-            <div  style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-                }}>
-            <IonButton tab="insta" href="https://www.instagram.com/37100lab.comunediverona/" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
-              <IonIcon size="large" icon={logoInstagram} />
-              <IonLabel></IonLabel>
-            </IonButton>
-            </div>
-
-            <div  style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-                }}>
-                  <IonButton tab="linkedin" href="https://www.linkedin.com/company/37100lab-comune-di-verona/" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
-              <IonIcon size="large" icon={logoLinkedin} />
-              <IonLabel></IonLabel>
-            </IonButton>
-            </div>
-              
-            <div  style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-                }}>
-                  <IonButton tab="youtube" href="https://www.youtube.com/channel/UCuUl-_yGTbId8juRTmhZ9EA" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
-              <IonIcon size="large" icon={logoYoutube} />
-              <IonLabel></IonLabel>
-            </IonButton>
-            </div>
+     </IonToolbar>
+     
+      <IonContent >
+    
+          <IonToolbar  color="success">
           
-            </IonToolbar>
-           
-        
-            <IonCard color="light">
-          <IonItem>
-            <IonIcon icon={earthOutline} slot="start" />
-            <IonLabel color="dark">PROSSIMI EVENTI 37100 COMUNE DI VERONA</IonLabel>
-            <IonButton href="https://37100lab.comune.verona.it/eventi/" fill="outline" slot="end">View</IonButton>
-          </IonItem>
+            <div  style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+              }}>
+          <IonButton href="https://www.facebook.com/37100Lab.ComuneVerona" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
+            <IonIcon size="large" icon={ logoFacebook} />
+            <IonLabel ></IonLabel>
+          </IonButton>
+          </div>
 
-          <IonCardContent>
-            Gli Innovation Lab sono dei veri e propri luoghi fisici che ospitano attività di co-progettazione e collaborazione tra soggetti pubblici e privati,
-          corsi di formazione per accrescere la cultura digitale, biblioteca e postazioni di co-working con accesso Wi-Fi, servizi di digital empowerment per le imprese, sportello per le politiche attive del lavoro, laboratori digitali, showroom dell’innovazione, servizi alle imprese e al territorio e alle start-up innovative.
-         L'idea principale nasce dall'esperienza e dalla capitalizzazione di precedenti azioni basate sul modello di Open Innovation. 
+          <div  style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+              }}>
+          <IonButton tab="insta" href="https://www.instagram.com/37100lab.comunediverona/" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
+            <IonIcon size="large" icon={logoInstagram} />
+            <IonLabel></IonLabel>
+          </IonButton>
+          </div>
+
+          <div  style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+              }}>
+                <IonButton tab="linkedin" href="https://www.linkedin.com/company/37100lab-comune-di-verona/" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
+            <IonIcon size="large" icon={logoLinkedin} />
+            <IonLabel></IonLabel>
+          </IonButton>
+          </div>
             
-      </IonCardContent>
-        </IonCard>
-         
-        </IonContent>
-
-        <IonFooter>
+          <div  style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+              }}>
+                <IonButton tab="youtube" href="https://www.youtube.com/channel/UCuUl-_yGTbId8juRTmhZ9EA" color="dark" shape="round" fill="outline" class="ion-justify-content-center">
+            <IonIcon size="large" icon={logoYoutube} />
+            <IonLabel></IonLabel>
+          </IonButton>
+          </div>
         
-        </IonFooter>
+          </IonToolbar>
+         
+      
+          <IonCard color="light">
+        <IonItem>
+          <IonIcon icon={earthOutline} slot="start" />
+          <IonLabel color="dark" className="ion-text-wrap">PROSSIMI EVENTI 37100 COMUNE DI VERONA</IonLabel>
+          <IonButton href="https://37100lab.comune.verona.it/eventi/" fill="outline" slot="end">Link</IonButton>
+        </IonItem>
 
-      </IonPage>
-    )
-  }
-  
-  export default AboutUs
-  
+        <IonItem>
+          <IonIcon icon={earthOutline} slot="start" />
+          <IonLabel color="dark" className="ion-text-wrap">CHI SIAMO</IonLabel>
+        </IonItem>
+
+        <IonCardContent>
+          Gli Innovation Lab sono dei veri e propri luoghi fisici che ospitano attività di co-progettazione e collaborazione tra soggetti pubblici e privati,
+          corsi di formazione per accrescere la cultura digitale, biblioteca e postazioni di co-working con accesso Wi-Fi, servizi di digital empowerment per le imprese, sportello per le politiche attive del lavoro, laboratori digitali, showroom dell’innovazione, servizi alle imprese e al territorio e alle start-up innovative.
+          L'idea principale nasce dall'esperienza e dalla capitalizzazione di precedenti azioni basate sul modello di Open Innovation. 
+        </IonCardContent>
+      </IonCard>         
+      </IonContent>
+
+    </IonPage>
+  )
+}
+
+export default AboutUs
+
+
