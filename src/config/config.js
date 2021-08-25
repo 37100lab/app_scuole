124d8a65b787938835e7538bdcd8afc40e0a47a8
xXAleXPolYXx <ale98sayan@gmail.com>
2021-08-25 10:02:17 +0200

    add Icons and Splash Page
    removed maxFeatures=50, modified localhost link

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const url =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:farmacie&outputFormat=application/json'
-    : `https://37100lab.it/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:scuole&maxFeatures=50&outputFormat=application/json`
+    ? 'http://localhost:8080/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:scuole&outputFormat=application/json'
+    : `https://37100lab.it/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:scuole&outputFormat=application/json`
